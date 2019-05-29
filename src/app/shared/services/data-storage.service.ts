cd2673b7b2ecf10b3b94e8c6273cd7c0dcbd66c8
thanos <imran.khan@synesisit.com.bd>
2019-05-29 12:42:48 +0600

    httpClient configuration added on data storage service

--- src/app/shared/services/data-storage.service.ts
+++ src/app/shared/services/data-storage.service.ts
@@ -6,35 +6,37 @@ import { catchError, map, tap } from 'rxjs/operators';
 import { AuthService } from 'src/app/auth/auth.service';
 @Injectable()
 export class DataStorageService {
-    constructor(private http: HttpClient,
-        private recipeService: RecipeService,
-        private authService: AuthService) { }
-    storeRecipe() {
-        const token = this.authService.getUserToken();
-        return this.http.put('https://ng-recipe-book-a48f4.firebaseio.com/recipe.json?auth='+token, this.recipeService.getRecipes())
-    }
-    retriveRecipe() {
-        const token = this.authService.getUserToken();
-        return this.http.get<Recipe[]>('https://ng-recipe-book-a48f4.firebaseio.com/recipe.json?auth='+token)
-            .pipe(
-                map( // is for formatting the data
-                    (recipes: Recipe[]) => {
-                        for (let recipe of recipes) {
-                            if (!recipe.ingredients) {// this is for handle the data inconsistency error 
-                                console.log(recipe)
-                                recipe.ingredients = [];
-                            }
-                        }
-                        return recipes;
-                    }
-                ),
-                tap()
-            )
+  constructor(private httpClient: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService) {
 
-            .subscribe(
-                (recipes: Recipe[]) => {
-                    this.recipeService.setRecipes(recipes);
-                }
-            )
-    }
+  }
+  storeRecipe() {
+    const token = this.authService.getUserToken();
+    return this.httpClient.put('https://ng-recipe-book-a48f4.firebaseio.com/recipe.json?auth=' + token, this.recipeService.getRecipes())
+  }
+  retriveRecipe() {
+    const token = this.authService.getUserToken();
+    return this.httpClient.get<Recipe[]>('https://ng-recipe-book-a48f4.firebaseio.com/recipe.json?auth=' + token, {
+      observe: 'body',
+      responseType: 'json'
+    }).pipe(
+      map( // is for formatting the data
+        (recipes) => {
+          for (let recipe of recipes) {
+            if (!recipe.ingredients) {// this is for handling the data presentation, because there is property inconsistency in database
+              console.log(recipe)
+              recipe.ingredients = [];
+            }
+          }
+          return recipes;
+        }
+      ),
+      tap()
+    ).subscribe(
+      (recipes: Recipe[]) => {
+        this.recipeService.setRecipes(recipes);
+      }
+    )
+  }
 }
